b5e70232df533da3536151f2f03011915dd50531
Sam Wu <swu@nait.ca>
2023-03-13 14:47:39 -0600

    Added Client Server WestWind project

--- RazorPagesDemo/RazorPagesDemo/Pages/ContactMe.cshtml.cs
+++ RazorPagesDemo/RazorPagesDemo/Pages/ContactMe.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace RazorPagesDemo.Pages
 {
@@ -33,41 +34,63 @@ namespace RazorPagesDemo.Pages
 
         public void OnPostSendMessage()
         {
-            string subscribeToMail = (SubscribeToMail == true) ? "Yes" : "No";
-            InfoMessage = $"Name: {ContactName} <br />"
-                + $"Email: {ContactEmail} <br />"
-                + $"Comments: {ContactComments} <br />"
-                + $"Subscribe to mail: {subscribeToMail}";
-
-            SmtpClient sendMailClient = new();
-            sendMailClient.Host = Configuration["MailServerSettings:SmtpHost"];
-            sendMailClient.Port = int.Parse(Configuration["MailServerSettings:Port"]);
-            sendMailClient.EnableSsl = bool.Parse(Configuration["MailServerSettings:EnableSsl"]);
-
-            NetworkCredential mailCredentials = new();
-            mailCredentials.UserName = Configuration["MailServerSettings:Username"];
-            mailCredentials.Password = Configuration["MailServerSettings:AppPassword"];
-            sendMailClient.Credentials = mailCredentials;
-
-            string mailToAddress = ContactEmail ?? Configuration["MailServerSettings:Email"];
-            string mailFromAddress = Configuration["MailServerSettings:Email"];
-
-            MailMessage mailMessage = new MailMessage(mailFromAddress, mailToAddress);
-            mailMessage.Subject = "CPSC1517 new contact me form submission";
-            mailMessage.Body = InfoMessage;
-
-            try
+            // Validate all form field values
+            if (string.IsNullOrWhiteSpace(ContactName))
             {
-                sendMailClient.Send(mailMessage);
-                ContactName = null;
-                ContactEmail = null;
-                ContactComments = null;
-                InfoMessage = "Your message has been sent";
+                ModelState.AddModelError(nameof(ContactName), "Contact Name is required and cannot be blank.");
             }
-            catch(Exception ex)
+            else if (ContactName.Length < 5)
             {
-                ErrorMessage = $"Error sending mail with exception: {ex.Message}";
+                ModelState.AddModelError(nameof(ContactName), "Contact Name must contain 5 or more characters.");
             }
+            if (string.IsNullOrWhiteSpace(ContactEmail))
+            {
+                ModelState.AddModelError(nameof(ContactEmail), "Contact Email is required and cannot be blank.");
+            }
+            //else if(Regex.Match(ContactEmail, "\A[A-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Z0-9.-]+\Z").)
+            if (string.IsNullOrWhiteSpace(ContactComments))
+            {
+                ModelState.AddModelError(nameof(ContactComments), "Contact Comments is required and cannot be blank.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                string subscribeToMail = (SubscribeToMail == true) ? "Yes" : "No";
+                InfoMessage = $"Name: {ContactName} <br />"
+                    + $"Email: {ContactEmail} <br />"
+                    + $"Comments: {ContactComments} <br />"
+                    + $"Subscribe to mail: {subscribeToMail}";
+
+                SmtpClient sendMailClient = new();
+                sendMailClient.Host = Configuration["MailServerSettings:SmtpHost"];
+                sendMailClient.Port = int.Parse(Configuration["MailServerSettings:Port"]);
+                sendMailClient.EnableSsl = bool.Parse(Configuration["MailServerSettings:EnableSsl"]);
+
+                NetworkCredential mailCredentials = new();
+                mailCredentials.UserName = Configuration["MailServerSettings:Username"];
+                mailCredentials.Password = Configuration["MailServerSettings:AppPassword"];
+                sendMailClient.Credentials = mailCredentials;
+
+                string mailToAddress = ContactEmail ?? Configuration["MailServerSettings:Email"];
+                string mailFromAddress = Configuration["MailServerSettings:Email"];
+
+                MailMessage mailMessage = new MailMessage(mailFromAddress, mailToAddress);
+                mailMessage.Subject = "CPSC1517 new contact me form submission";
+                mailMessage.Body = InfoMessage;
+
+                try
+                {
+                    sendMailClient.Send(mailMessage);
+                    ContactName = null;
+                    ContactEmail = null;
+                    ContactComments = null;
+                    InfoMessage = "Your message has been sent";
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage = $"Error sending mail with exception: {ex.Message}";
+                }
+            }            
 
         }
         public IActionResult OnPostClear()
---
+++ WestWindSolution/WestWindSystem/DAL/WestWindContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.DAL
+{
+    internal class WestWindContext : DbContext
+    {
+        public WestWindContext(DbContextOptions<WestWindContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BuildVersion> BuildVersions => Set<BuildVersion>();
+    }
+
+}
